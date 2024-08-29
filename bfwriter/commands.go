79f49f73de2895a525e1872cc4a4f4a3f2a6e058
angrykoala <angrykoala@outlook.es>
2024-08-29 21:45:13 +0100

    Some more algos

--- bfwriter/commands.go
+++ bfwriter/commands.go
@@ -1,5 +1,8 @@
 package bfwriter
 
+// Based on https://esolangs.org/wiki/Brainfuck_algorithms
+// and https://gist.github.com/roachhd/dce54bec8ba55fb17d3a
+
 type CommandHandler struct {
 	writer          *writer
 	lastPointerMove int
@@ -68,8 +71,12 @@ func (c *CommandHandler) MovePointer(pos int) {
 }
 
 // Move to previous pointer
-func (c *CommandHandler) Pop() {
+func (c *CommandHandler) pop() {
+	if c.lastPointerMove == 0 {
+		panic("Cannot Pop, no last Pointer moved")
+	}
 	c.MovePointer(-c.lastPointerMove)
+	c.lastPointerMove = 0
 }
 
 // Move the value of current cell to target cell (counting from current cell)
@@ -79,39 +86,75 @@ func (c *CommandHandler) Shift(to int) {
 	c.BeginLoop()
 	c.MovePointer(to)
 	c.Inc()
-	c.Pop()
+	c.pop()
 	c.Dec()
 	c.EndLoop()
 }
 
-// y[x+temp0+y-]
-// temp0[y+temp0-]
-
-// Copy current cell into to, using temp, ends in origin and resets temp
+// Copy current cell into to, using temp cell, ends in origin and resets temp
 func (c *CommandHandler) Copy(to int, temp int) {
 	// Reset temp and to
 	c.MovePointer(temp)
 	c.Reset()
-	c.Pop()
+	c.pop()
 	c.MovePointer(to)
 	c.Reset()
-	c.Pop()
+	c.pop()
 
 	// Copy origin to to and temp
 	c.BeginLoop()
 	c.MovePointer(to)
 	c.Inc()
-	c.Pop()
+	c.pop()
 	c.MovePointer(temp)
 	c.Inc()
-	c.Pop()
+	c.pop()
 	c.Dec()
 	c.EndLoop()
 
 	// Shift temp to origin
 	c.MovePointer(temp)
 	c.Shift(-temp)
+}
 
+// Adds cell to current cell, using temp
+func (c *CommandHandler) AddCell(y int, temp int) {
+	// Reset temp and to
+	c.MovePointer(temp)
+	c.Reset()
+	c.pop()
+
+	c.MovePointer(y)
+	// Increment x and temp with y
+	c.BeginLoop() // Loop begins at y
+	c.pop()
+	c.Inc()
+	c.MovePointer(temp)
+	c.Inc()
+	c.pop()
+	c.MovePointer(y)
+	c.Dec()
+	c.EndLoop()
+
+	// Move temp to y
+	c.pop()
+	c.MovePointer(temp)
+	c.Shift(y - temp)
+}
+
+// Rus code inside if the current cell is truthy
+// Accepts a code function using the command handler
+// The resulting function must end in the same position as it begins!
+func (c *CommandHandler) If(temp int, code func()) {
+	c.MovePointer(temp)
+	c.Reset()
+	c.pop()
+
+	c.BeginLoop()
+	code()
+	c.MovePointer(temp)
+	c.EndLoop()
+	c.pop()
 }
 
 func (c *CommandHandler) Comment(comment string) {
--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import "mindfck/bfwriter"
 func main() {
 	cmd := bfwriter.NewCommandHandler()
 
-	cmd.Add(10)
-	cmd.Shift(10)
-	cmd.MovePointer(10)
-	cmd.Comment("Move Pointer")
-	cmd.Copy(-2, -9)
-	cmd.Comment("Copy")
+	// cmd.MovePointer(2)
+	// cmd.Add(10)
+	// cmd.MovePointer(1)
+	// cmd.Add(5)
+	// cmd.MovePointer(-1)
+	// cmd.Comment("Setup")
+	// cmd.AddCell(1, -1)
+	// cmd.Comment("Add cell 2 using 3 as temp")
+
+	cmd.Add(1)
+	cmd.If(2, func() {
+		cmd.Add(2)
+	})
+
+	// cmd.Shift(10)
+	// cmd.Copy(-2, -9)
+	// cmd.Comment("Copy")
+
 	cmd.Print()
 }
 
