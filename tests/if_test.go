0cd8b811557130ff8eb3b9d070a03d602eefa176
angrykoala <angrykoala@outlook.es>
2024-09-12 22:19:26 +0100

    Add nested if test

--- tests/if_test.go
+++ tests/if_test.go
@@ -50,3 +50,29 @@ func TestIfFalse(t *testing.T) {
 	interpreter.Run(code)
 	assert.Equal(t, []byte{0}, interpreter.Output)
 }
+
+func TestNestedIf(t *testing.T) {
+	input := `
+	byte a
+	byte b
+	a = 2
+	b = 3
+    if (a==2) {
+        print a
+		if (a+b==5) {
+			print 6
+		}
+    }
+    print 0
+	`
+
+	ast, err := parser.Parse(input)
+	assert.Nil(t, err)
+
+	code, err := compiler.Compile(ast)
+	assert.Nil(t, err)
+
+	interpreter := bfinterpreter.New()
+	interpreter.Run(code)
+	assert.Equal(t, []byte{2, 6, 0}, interpreter.Output)
+}
