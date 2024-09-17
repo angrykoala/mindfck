59998c093273a9376d1438569773977791b8b640
angrykoala <angrykoala@outlook.es>
2024-09-17 17:05:53 +0100

    Add extra test with not and grouping

--- tests/logical_expr_test.go
+++ tests/logical_expr_test.go
@@ -31,3 +31,23 @@ func TestLogicalExpressions(t *testing.T) {
 	interpreter.Run(code)
 	assert.Equal(t, []byte{0, 0, 1, 1}, interpreter.Output)
 }
+
+func TestLogicalExpressionsGrouping(t *testing.T) {
+	input := `
+	print !(1 == 1 and 1 == 0)
+	`
+
+	ast, err := parser.Parse(input)
+	if err != nil {
+		panic(err)
+	}
+
+	code, err := compiler.Compile(ast)
+	if err != nil {
+		panic(err)
+	}
+
+	interpreter := bfinterpreter.New()
+	interpreter.Run(code)
+	assert.Equal(t, []byte{1}, interpreter.Output)
+}
